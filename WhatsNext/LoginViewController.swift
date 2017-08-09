26895862a0c72a92e30e518f86ac8f868f3fe459
Tamer Bader <tbader@terpmail.umd.edu>
2017-08-08 23:07:43 -0400

    Updated Login UI

--- WhatsNext/LoginViewController.swift
+++ WhatsNext/LoginViewController.swift
@@ -9,11 +9,52 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    let TEN_PERCENT_CONSTRAINT_VALUE: CGFloat = 0.1
+    let FIVE_PERCENT_CONSTRAINT_VALUE: CGFloat = 0.05
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var emailUnderline: UIView!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordUnderline: UIView!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpLabel: UILabel!
+
+    @IBOutlet weak var formView: UIView!
+    
+    @IBOutlet weak var fieldsSpacingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var signInTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var signUpMessageConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupView()
+        
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func setupView() {
+        emailField.attributedPlaceholder = NSAttributedString(string: "Email Address", attributes: [NSForegroundColorAttributeName: UIColor.black])
+        passwordField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSForegroundColorAttributeName: UIColor.black])
+        fieldsSpacingConstraint.constant = formView.frame.height * TEN_PERCENT_CONSTRAINT_VALUE
+        signInTopConstraint.constant = formView.frame.height * TEN_PERCENT_CONSTRAINT_VALUE
+        signUpMessageConstraint.constant = formView.frame.height * FIVE_PERCENT_CONSTRAINT_VALUE
+        
+        let signUpMessage:NSMutableAttributedString = NSMutableAttributedString(string: "")
+        
+        let questionAttributedMessage:NSAttributedString = NSAttributedString(string: "DON'T HAVE AN ACCOUNT?", attributes: [NSForegroundColorAttributeName: UIColor.Gray])
+        let signUpAttributedMessage:NSAttributedString = NSAttributedString(string: "  SIGN UP", attributes: [NSForegroundColorAttributeName: UIColor.Black])
+        signUpMessage.append(questionAttributedMessage)
+        signUpMessage.append(signUpAttributedMessage)
+        
+        signUpLabel.attributedText = signUpMessage
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +62,8 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func signIn(_ sender: UIButton) {
+        
     }
-    */
 
 }
---
+++ WhatsNext/WhatsNext/UIColor.swift
@@ -0,0 +1,26 @@
+//
+//  UIColor.swift
+//  WhatsNext
+//
+//  Created by MetroStar on 8/8/17.
+//  Copyright © 2017 TamerBader. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    class var Gray:UIColor {
+        return UIColor(red: 142/255, green: 142/255, blue: 146/255, alpha: 1.0)
+    }
+    class var OpaqueGray:UIColor {
+        return UIColor(red: 142/255, green: 142/255, blue: 146/255, alpha: 0.5)
+
+    }
+    
+    class var Black: UIColor {
+        return UIColor(red: 29/255, green: 29/255, blue: 38/255, alpha: 1.0)
+    }
+    
+}
